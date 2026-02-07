5843975748730cc5249589e0e4a111d5483625f6
sivakumar <sivakumarthalla123@gmail.com>
2026-02-07 12:20:57 +0530

    variables

---
+++ backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+log_folder="/home/ec2-uer/app.log"
+log_file="$log_folder/$0.log"
+
+if [ ! -d $log_folder ]; then
+   echo "this folder $log_folder is not exist.."
+   exit 1
+fi
+file_to_delete=$(find $log_folder -name "*.log" -type f -mtime +14) 
+
+echo "$file_to_delete"
