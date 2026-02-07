f52ceedae65db28fb2f289354c1dd8528dc14dce
sivakumar <sivakumarthalla123@gmail.com>
2026-02-07 13:44:03 +0530

    variables

--- backup1.sh
+++ backup1.sh
@@ -53,9 +53,9 @@ else
 
     timestampt=$(date "+%F %h-%m-%s")
     zip_file_name="$destinationdirectory/app.logs-$timestampt.tar.gz"
-    find $sourcedirectory -name "*.log" -type f -mtime $numberofdays | tar -zcvf $zip_file_name
+    find $sourcedirectory -name "*.log" -type f -mtime +$numberofdays | tar -zcvf $zip_file_name
 
-   if [ -f $zip_file_name]; then
+   if [ -f $zip_file_name ]; then
         log "archiving is success.."
         while IFS= read -r filepath; do
          log "Deleting file: $filepath"
